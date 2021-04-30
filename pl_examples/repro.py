15863f287519daf0dfe052a078d391a2425bbfc6
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-04-30 02:04:58 +0200

    rep

---
+++ pl_examples/repro.py
@@ -0,0 +1,64 @@
+import os
+from argparse import ArgumentParser
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from pytorch_lightning import LightningModule, Trainer
+
+
+class RandomDataset(Dataset):
+
+    def __init__(self, size, length):
+        self.len = length
+        self.data = torch.randn(length, size)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __len__(self):
+        return self.len
+
+
+class BoringModel(LightningModule):
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.save_hyperparameters(kwargs)
+        self.layer = torch.nn.Linear(32, 2)
+
+    def forward(self, x):
+        return self.layer(x)
+
+    def training_step(self, batch, batch_idx):
+        loss = self(batch).sum()
+        self.log("train_loss", loss)
+        return {"loss": loss}
+
+    def validation_step(self, batch, batch_idx):
+        loss = self(batch).sum()
+        self.log("valid_loss", loss)
+
+    def test_step(self, batch, batch_idx):
+        loss = self(batch).sum()
+        self.log("test_loss", loss)
+
+    def configure_optimizers(self):
+        return torch.optim.SGD(self.layer.parameters(), lr=0.1)
+
+
+def run():
+    train_data = DataLoader(RandomDataset(32, 64), batch_size=2)
+    val_data = DataLoader(RandomDataset(32, 64), batch_size=2)
+    test_data = DataLoader(RandomDataset(32, 64), batch_size=2)
+
+    parser = ArgumentParser()
+    parser = Trainer.add_argparse_args(parser)
+    args = parser.parse_args()
+    trainer = Trainer.from_argparse_args(args)
+    model = BoringModel(**vars(args))
+    trainer.fit(model, train_dataloader=train_data, val_dataloaders=val_data)
+    trainer.test(model, test_dataloaders=test_data)
+
+
+if __name__ == '__main__':
+    run()

039703869820a1ed57ade275fab54287d23701fe
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-04-30 02:09:19 +0200

    x

--- pl_examples/repro.py
+++ pl_examples/repro.py
@@ -3,7 +3,8 @@ from argparse import ArgumentParser
 
 import torch
 from torch.utils.data import Dataset, DataLoader
-from pytorch_lightning import LightningModule, Trainer
+from pytorch_lightning import LightningModule, Trainer, seed_everything
+from pytorch_lightning.loggers import WandbLogger
 
 
 class RandomDataset(Dataset):
@@ -47,18 +48,24 @@ class BoringModel(LightningModule):
 
 
 def run():
-    train_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-    val_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-    test_data = DataLoader(RandomDataset(32, 64), batch_size=2)
+    train_data = DataLoader(RandomDataset(32000, 64), batch_size=2)
+    val_data = DataLoader(RandomDataset(32000, 64), batch_size=2)
+    test_data = DataLoader(RandomDataset(32000, 64), batch_size=2)
 
     parser = ArgumentParser()
     parser = Trainer.add_argparse_args(parser)
+    parser.add_argument("--name", type=str, default="debug")
+    parser.set_defaults(
+        max_epochs=5,
+    )
     args = parser.parse_args()
-    trainer = Trainer.from_argparse_args(args)
+    logger = WandbLogger(project="ddp-partiy-1.3.0", name=args.name)
+    trainer = Trainer.from_argparse_args(args, logger=logger)
     model = BoringModel(**vars(args))
     trainer.fit(model, train_dataloader=train_data, val_dataloaders=val_data)
     trainer.test(model, test_dataloaders=test_data)
 
 
 if __name__ == '__main__':
+    seed_everything(0)
     run()

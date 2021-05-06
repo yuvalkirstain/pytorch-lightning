1efe9ff48ab268c412fe956865362669b295a96e
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-05-06 03:15:53 +0200

    x

--- pl_examples/thomas.py
+++ pl_examples/thomas.py
@@ -7,7 +7,7 @@ def main():
     print("init")
     print("rank", int(os.environ["RANK"]))
     torch.distributed.init_process_group(
-        backend="nccl",
+        backend="gloo",
         init_method="tcp://10.10.10.22:1191",
         rank=int(os.environ["RANK"]),
         world_size=2,

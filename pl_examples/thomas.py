6351fb181e18f0aee3002685a202309d7daeb45a
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-05-06 03:15:14 +0200

    x

--- pl_examples/thomas.py
+++ pl_examples/thomas.py
@@ -5,11 +5,12 @@ import torch.distributed
 
 def main():
     print("init")
+    print("rank", int(os.environ["RANK"]))
     torch.distributed.init_process_group(
         backend="nccl",
         init_method="tcp://10.10.10.22:1191",
         rank=int(os.environ["RANK"]),
-        world_size=2
+        world_size=2,
     )
     print("barrier")
     torch.distributed.barrier()

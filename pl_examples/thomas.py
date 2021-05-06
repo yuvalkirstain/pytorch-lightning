233caceeb3b5b4c8dfccd9053bd373fb86d0c1d1
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-05-06 04:20:49 +0200

    x

--- pl_examples/thomas.py
+++ pl_examples/thomas.py
@@ -4,6 +4,13 @@ import torch.distributed
 
 
 def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--local_rank", type=int)
+    args = parser.parse_args()
+
+    torch.cuda.set_device(args.local_rank)
+
     print("init")
     print("rank", int(os.environ["RANK"]))
     print("world size", int(os.environ["WORLD_SIZE"]))

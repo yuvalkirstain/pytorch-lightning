cdaadc6604b6871e3b3beefd2ebbe43d1d722707
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-05-10 21:31:09 +0200

    simple

--- pl_examples/thomas.py
+++ pl_examples/thomas.py
@@ -7,6 +7,7 @@ def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--local_rank", type=int)
+    parser.add_argument("--global_rank", type=int)
     args = parser.parse_args()
 
     torch.cuda.set_device(args.local_rank)
@@ -15,8 +16,10 @@ def main():
     print("rank", int(os.environ["RANK"]))
     print("world size", int(os.environ["WORLD_SIZE"]))
     torch.distributed.init_process_group(
-        backend="gloo",
-        init_method="env://",
+        backend="nccl",
+        init_method="tcp://10.10.10.22:1191",
+        world_size=2,
+        rank=args.global_rank,
     )
     print("barrier")
     torch.distributed.barrier()

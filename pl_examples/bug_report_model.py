a60291e28413dc081faf85aa94eaabc55126966f
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-05-11 10:20:16 +0200

    x

--- pl_examples/bug_report_model.py
+++ pl_examples/bug_report_model.py
@@ -49,24 +49,8 @@ class BoringModel(LightningModule):
 def run():
     train_data = DataLoader(RandomDataset(32, 64), batch_size=2)
     val_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-    test_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-
-    # hvd.init()
 
     model = BoringModel()
-
-    # def _filter_named_parameters(model, optimizer):
-    #     opt_params = set([p for group in optimizer.param_groups for p in group.get("params", [])])
-    #     return [(name, p) for name, p in model.named_parameters() if p in opt_params]
-    #
-    # optim = model.configure_optimizers()
-    # hvd.DistributedOptimizer(
-    #     optimizer=optim,
-    #     named_parameters=_filter_named_parameters(model, optim),
-    #     compression=hvd.Compression.none,
-    # )
-
-
     trainer = Trainer(
         default_root_dir=os.getcwd(),
         limit_train_batches=10,
@@ -77,8 +61,9 @@ def run():
         gpus=1,
     )
     trainer.fit(model, train_dataloader=train_data, val_dataloaders=val_data)
-    # trainer.test(model, test_dataloaders=test_data)
 
 
 if __name__ == '__main__':
+    # run on two gpus:
+    # horovodrun -np 2 python bug_report_model.py
     run()

6f56f2e7e8b3e08a5c4174ad8877b54025164cf8
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-06-22 18:20:56 +0200

    debug

--- pl_examples/bug_report_model.py
+++ pl_examples/bug_report_model.py
@@ -1,67 +1,61 @@
-import os
-
 import torch
-from torch.utils.data import DataLoader, Dataset
-
-from pytorch_lightning import LightningModule, Trainer
-
-
-class RandomDataset(Dataset):
-
-    def __init__(self, size, length):
-        self.len = length
-        self.data = torch.randn(length, size)
-
-    def __getitem__(self, index):
-        return self.data[index]
-
-    def __len__(self):
-        return self.len
+from torch import nn
+import torch.nn.functional as F
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+import pytorch_lightning as pl
 
 
-class BoringModel(LightningModule):
+class LitAutoEncoder(pl.LightningModule):
 
     def __init__(self):
         super().__init__()
-        self.layer = torch.nn.Linear(32, 2)
+        self.encoder = nn.Sequential(nn.Linear(28 * 28, 128), nn.ReLU(), nn.Linear(128, 3))
+        self.decoder = nn.Sequential(nn.Linear(3, 128), nn.ReLU(), nn.Linear(128, 28 * 28))
 
     def forward(self, x):
-        return self.layer(x)
+        embedding = self.encoder(x)
+        return embedding
 
     def training_step(self, batch, batch_idx):
-        loss = self(batch).sum()
-        self.log("train_loss", loss)
-        return {"loss": loss}
-
-    def validation_step(self, batch, batch_idx):
-        loss = self(batch).sum()
-        self.log("valid_loss", loss)
-
-    def test_step(self, batch, batch_idx):
-        loss = self(batch).sum()
-        self.log("test_loss", loss)
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = F.mse_loss(x_hat, x)
+        self.log('train_loss', loss)
+        # self.trainer.profiler.describe()  # why was this here??
+        return loss
 
     def configure_optimizers(self):
-        return torch.optim.SGD(self.layer.parameters(), lr=0.1)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
 
 
-def run():
-    train_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-    val_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-    test_data = DataLoader(RandomDataset(32, 64), batch_size=2)
+dataset = MNIST("./MNIST", download=True, transform=transforms.ToTensor())
+train, val = random_split(dataset, [55000, 5000])
 
-    model = BoringModel()
-    trainer = Trainer(
-        default_root_dir=os.getcwd(),
-        limit_train_batches=1,
-        limit_val_batches=1,
-        num_sanity_val_steps=0,
-        max_epochs=1,
-        weights_summary=None,
+autoencoder = LitAutoEncoder()
+
+
+settings = [
+    # accumulation, max_steps
+    (1, 1000),
+    (4, 250),
+    (8, 125),
+    (8, 250),
+    (8, 500),
+    (8, 1000),
+]
+
+for accumulation, max_steps in settings:
+    trainer = pl.Trainer(
+        profiler="simple",
+        accumulate_grad_batches=accumulation,
+        log_every_n_steps=50,
+        max_steps=max_steps,
     )
-    trainer.fit(model, train_dataloader=train_data, val_dataloaders=val_data)
-    trainer.test(model, test_dataloaders=test_data)
-
-
-if __name__ == '__main__':
-    run()
+    trainer.profiler.dirpath = "lightning_logs"
+    trainer.profiler.filename = f'fprofiler_accumulation_{accumulation}_{max_steps}'
+    trainer.fit(autoencoder, DataLoader(train), DataLoader(val))

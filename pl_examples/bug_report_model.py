4eb14b0e1a1bdb40e22a09eded6b6dbe788fcdbe
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-07-16 13:58:12 +0200

    garbage collection

--- pl_examples/bug_report_model.py
+++ pl_examples/bug_report_model.py
@@ -1,67 +1,114 @@
-import os
+import gc
 
 import torch
-from torch.utils.data import DataLoader, Dataset
+from pytorch_lightning.utilities.memory import garbage_collection_cuda
+    import time
 
-from pytorch_lightning import LightningModule, Trainer
+def get_model():
+    from pytorch_lightning import LightningModule
 
+    import torch
+    from torch import nn
+    from torch.utils.data import DataLoader, Dataset
 
-class RandomDataset(Dataset):
+    class RandomDataset(Dataset):
+        def __init__(self, size, num_samples):
+            self.len = num_samples
+            self.data = torch.randn(num_samples, size)
 
-    def __init__(self, size, length):
-        self.len = length
-        self.data = torch.randn(length, size)
+        def __getitem__(self, index):
+            return self.data[index]
 
-    def __getitem__(self, index):
-        return self.data[index]
+        def __len__(self):
+            return self.len
 
-    def __len__(self):
-        return self.len
+    class BoringModel(LightningModule):
 
+        def __init__(self):
+            super().__init__()
+            self.layer = torch.nn.Linear(32, 2)
+            self.batch_size = 4
 
-class BoringModel(LightningModule):
+        def train_dataloader(self):
+            return DataLoader(RandomDataset(32, 10000),
+                            batch_size=self.batch_size)
 
-    def __init__(self):
-        super().__init__()
-        self.layer = torch.nn.Linear(32, 2)
+        def forward(self, x):
+            return self.layer(x)
 
-    def forward(self, x):
-        return self.layer(x)
+        def loss(self, batch, prediction):
+            # An arbitrary loss to have a loss that updates the model weights during `Trainer.fit` calls
+            return nn.MSELoss()(prediction, torch.ones_like(prediction))
 
-    def training_step(self, batch, batch_idx):
-        loss = self(batch).sum()
-        self.log("train_loss", loss)
-        return {"loss": loss}
+        def training_step(self, batch, batch_idx):
+            output = self.layer(batch)
+            loss = self.loss(batch, output)
+            return loss
 
-    def validation_step(self, batch, batch_idx):
-        loss = self(batch).sum()
-        self.log("valid_loss", loss)
+        def training_step_end(self, training_step_outputs):
+            return training_step_outputs
 
-    def test_step(self, batch, batch_idx):
-        loss = self(batch).sum()
-        self.log("test_loss", loss)
+        def training_epoch_end(self, outputs) -> None:
+            torch.stack([x["loss"] for x in outputs]).mean()
 
-    def configure_optimizers(self):
-        return torch.optim.SGD(self.layer.parameters(), lr=0.1)
-
-
-def run():
-    train_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-    val_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-    test_data = DataLoader(RandomDataset(32, 64), batch_size=2)
+        def configure_optimizers(self):
+            optimizer = torch.optim.SGD(self.layer.parameters(), lr=0.1)
+            lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1)
+            return [optimizer], [lr_scheduler]
 
     model = BoringModel()
-    trainer = Trainer(
-        default_root_dir=os.getcwd(),
-        limit_train_batches=1,
-        limit_val_batches=1,
-        num_sanity_val_steps=0,
-        max_epochs=1,
-        weights_summary=None,
-    )
-    trainer.fit(model, train_dataloaders=train_data, val_dataloaders=val_data)
-    trainer.test(model, dataloaders=test_data)
+    return model
+
+
+def get_cudas():
+    cudas = []
+
+    for obj in gc.get_objects():
+        try:
+            if torch.is_tensor(obj) or (hasattr(obj, 'data') and torch.is_tensor(obj.data)):
+                if str(obj.device) != 'cpu':
+                    cudas += [obj]
+        except:
+            pass
+    return cudas
+
+
+def run(precision=32, accumulate_grad_batches=1):
+        model = get_model()
+        import pytorch_lightning as pl
+
+        trainer = pl.Trainer(logger=True,
+                             gpus='0',
+                             accumulate_grad_batches=accumulate_grad_batches,
+                             log_every_n_steps=1,
+                             precision=precision,
+                             deterministic=True,
+                             max_steps=32,
+                             reload_dataloaders_every_epoch=True,
+                             auto_lr_find=False,
+                             replace_sampler_ddp=True,
+                             terminate_on_nan=False,
+                             auto_scale_batch_size=True,
+                             weights_summary=None,
+                             progress_bar_refresh_rate=0,
+                             )
+
+        trainer.fit(model)
 
 
 if __name__ == '__main__':
+    print("first run")
     run()
+    print("memory:", torch.cuda.memory_allocated(), "max:", torch.cuda.max_memory_allocated())
+
+    garbage_collection_cuda()
+    time.sleep(5)
+    torch.cuda.empty_cache()
+    garbage_collection_cuda()
+    gc.collect()
+
+    get_cudas()
+
+    print("second run")
+    run()
+    print("memory:", torch.cuda.memory_allocated(), "max:", torch.cuda.max_memory_allocated())
